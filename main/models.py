5e26673e4bc53fec9e6396ceff45501dcbf200fa
Moonsur <monsur.cs.it@gmail.com>
2021-02-19 00:44:10 +0600

    some change in admin.py and models.py

--- main/models.py
+++ main/models.py
@@ -7,6 +7,20 @@ class ActiveManager(models.Manager):
         return self.filter(active=True)
 
 
+class ProductTag(models.Model):
+    #products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def natural_key(self):
+        return self.slug
+
+
 class Product(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
@@ -15,6 +29,7 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(ProductTag, blank=True)
     objects = ActiveManager()
 
     def __str__(self):
@@ -28,14 +43,3 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return "Image of " + self.product.name
-
-
-class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=32)
-    slug = models.SlugField(max_length=48)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
